3fe5f5517592ca5870f4740dad8bc26058865a84
Olegechkin <olechka5.14@yandex.ru>
2023-04-29 23:59:13 +0400

    Добавлен класс Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(bean2.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat catBeen = (Cat) applicationContext.getBean("cat");
+//        System.out.println(catBeen.getMessage());
+        Cat catBeen2 = (Cat) applicationContext.getBean("cat");
+//        System.out.println(catBeen2.getMessage());
+        System.out.println(catBeen == catBeen2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+
+    private String message;
+
+    public Cat(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
+
+
+
+
